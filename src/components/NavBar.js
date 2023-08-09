45a19369a741ddf439892d411a32f5d3a3c52710
Jeyna Kim <jeyna.kimm@gmail.com>
2023-08-08 23:53:27 -0400

    fix span underline issue

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -18,7 +18,7 @@ const CustomLink = ({ href, title, className = '' }) => {
         className={`
       h-[1px] inline-block bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300
       ${router.asPath === href ? 'w-full' : 'w-0'}
-      dark:bg-dark`}
+      dark:bg-light`}
       >
         &nbsp;
       </span>
@@ -43,7 +43,7 @@ const CustomMobileLink = ({ href, title, className = '', toggle }) => {
       {title}
       <span
         className={`
-      h-[1px] inline-block bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300
+      h-[1px] inline-block bg-light absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300
       ${router.asPath === href ? 'w-full' : 'w-0'}
       dark:bg-dark`}
       >
